b982cfb6c9d3ccc1c5b1edb8d163138c5d0aec1e
excellis-it <web.excellisit@gmail.com>
2024-05-30 19:02:32 +0530

    ad

--- public/frontend_assets/email-php/grievance-form.php
+++ public/frontend_assets/email-php/grievance-form.php
@@ -7,9 +7,14 @@ require 'vendor/autoload.php';
 
 if (isset($_POST['submit'])) {
     // print_r($_POST);die();
-    $your_name = $_POST['name'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
+    $gender = $_POST['gender'];
     $phone = $_POST['phone'];
+    $reg = $_POST['registration'];
+    $prog = $_POST['programme'];
+    $cat = $_POST['category-grievance'];
+    $message = $_POST['message'];
     $mail = new PHPMailer(true);
     try {
         $mail->SMTPDebug = 0;
@@ -20,15 +25,21 @@ if (isset($_POST['submit'])) {
         $mail->Password = 'fzzafjvhehwahcmi';  //put your gmail password
         $mail->SMTPSecure = 'ssl';
         $mail->Port = 465;
-        $mail->setFrom("contact@msu.edu.in", 'Download Prospectus Form Details'); //send from
+        $mail->setFrom("contact@msu.edu.in", 'Grievance Redressal Form Details'); //send from
         $mail->addAddress("contact@msu.edu.in"); //send to   
         // $mail->AddCC("khurshid@pixelstreet.in");
         // $mail->AddCC("subhradeep.pixelstreet@gmail.com");
         $mail->isHTML(true);
-        $mail->Subject = "Download Prospectus Form Details";
-        $mail->Body = "<h1>Download Prospectus Form Details : </h1><h3>Your Name: " . $your_name . "</h3>
+        $mail->Subject = "Grievance Redressal Form Details";
+        $mail->Body = "<h1>Grievance Redressal Form Details : </h1><h3>Your Name: " . $name . "</h3>
             <h3>Email: " . $email . "</h3>
-            <h3>Phone: " . $phone . "</h3>";
+            <h3>Gender: " . $gender . "</h3>
+            <h3>Registration: " . $reg . "</h3>
+            <h3>Programme: " . $prog . "</h3>
+            <h3>Category Grievance: " . $cat . "</h3>
+            <h3>Email: " . $email . "</h3>
+            <h3>Phone: " . $phone . "</h3>
+            <h3>Message: " . $message . "</h3>";
 
         if ($mail->Send()) {
             header('Location: https://www.msu.edu.in/thank-you');
